488070c8e4c6978e2252850aded54ed8254b17bf
Hava GÜNDOĞAN <76647908+HavaGundogan@users.noreply.github.com>
2022-11-03 13:53:21 +0300

    created

---
+++ lesson5/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,32 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Brand {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "name")
+	private String name;
+	// veritabanında tutulacak verilere ulaşmamız lazım
+	// bunu getirmek ya da eklemek için bu işlemleri dataAccess de yapacağız.
+
+}
